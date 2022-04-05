5fc2b8a3c66cce856817b4c8a251c88622c17e0c
Luccas Lombardi <luccas.lombardi@hotmail.com>
2022-04-04 21:44:13 -0300

    feat: consumo de API

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import api from "./services/api"
 
 export default function App() {
+
+  const getProducts = () => {
+    const result = api.get('/');
+    console.log(result)
+  }
+
+  getProducts()
+
   return (
     <View style={styles.container}>
-      <Text>Hello World! This is LuExpress...</Text>
+      <Text>Hello World! This is LuExpress</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
