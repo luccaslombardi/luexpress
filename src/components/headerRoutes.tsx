cea0ab29f40f7df7f31d71cf7c600fac8499bf37
luccaslombardi <luccas.lombardi@hotmail.com>
2022-04-10 16:08:43 -0300

    feat: Estilização home page

--- src/components/headerRoutes.tsx
+++ src/components/headerRoutes.tsx
@@ -2,25 +2,65 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../pages/home';
 import Cart from '../pages/cart';
+import { Image, Text, StyleSheet } from "react-native";
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading'
 
 const Stack = createStackNavigator()
 
 function HeaderRoutes() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    });
+
+    let [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
+
     return (
         <Stack.Navigator>
             <Stack.Screen
                 name="home"
+
                 component={Home}
                 options={{
-                    title: "HOME",
+
+                    headerTitle: () => {
+                        return (
+                            <Image
+                                source={require("../../assets/logo.png")}
+                                style={{ width: 150, height: 70 }}
+                            />
+                        )
+
+                    },
+                    headerRight: () => {
+                        return (
+                            <Text style={styles.date}>{currentDate}</Text>
+                        )
+                    }
                 }}
+
             />
 
             <Stack.Screen
                 name="cart"
                 component={Cart}
                 options={{
-                    title: "CART"
+                    title: "CART",
+                    headerRight: () => {
+                        return (
+                            <Text style={styles.date}>{currentDate}</Text>
+                        )
+                    }
+
                 }}
             />
 
@@ -32,3 +72,12 @@ function HeaderRoutes() {
 }
 
 export default HeaderRoutes
+
+const styles = StyleSheet.create({
+    date: {
+        fontFamily: "Roboto_400Regular",
+        fontSize: 18,
+        paddingRight: 15,
+        color: "#54457F"
+    }
+})
--- src/content/product.tsx
+++ src/content/product.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
-import { Text, Image, View, StyleSheet, TouchableOpacity, Button, Vibration } from "react-native"
+import { Text, Image, View, StyleSheet, TouchableOpacity, Button } from "react-native"
 import { useNavigation } from '@react-navigation/native'
-import { AntDesign } from '@expo/vector-icons'
+import { AntDesign, Ionicons } from '@expo/vector-icons'
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading'
 
 interface productData {
     id: number;
@@ -23,6 +25,8 @@ function Products() {
     const cartItems: productData[] = []
     const navigator = useNavigation()
 
+
+
     useEffect(() => {
         api
             .get("/products")
@@ -35,66 +39,95 @@ function Products() {
 
 
     const addToCart = async (lastProducts: any, product: any) => {
-        lastProducts.push(product)
-    }
+        const findItem = cartItems.find(item => item.id == product.id)
 
-    const removeFromCart = async (idItem: number) => {
-        const i = cartItems;
-        const aux = i.findIndex((item: { id: number; }) => item.id === idItem);
-        i.splice(aux, 1);
-    }
+        if (findItem === undefined) {
+            lastProducts.push(product)
 
-    const buttonOnPress = async (product: any, productId: number) => {
-        const aux = cartItems.find(item => item.id == productId)
-
-        if (aux === undefined) {
-            addToCart(cartItems, product);
-            console.log(cartItems)
-        } else if (aux.id === productId) {
-            removeFromCart(productId)
-            console.log(cartItems)
-
+        } else if (findItem.id === product.id) {
+            alert("produto já esta no carrinho")
         }
+
+
+
+    }
+
+    const removeFromCart = async (product: any) => {
+
+        const findItem = cartItems.find(item => item.id == product.id)
+
+        if (findItem === undefined) {
+            alert("Esse produto não está no carrinho")
+        } else if (findItem.id === product.id) {
+            const i = cartItems;
+            const aux = i.findIndex((item: { id: number; }) => item.id === product.id);
+            i.splice(aux, 1);
+        }
+    }
+
+    let [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
     }
 
     return (
 
+        <View>
+            <View style={styles.firstContainer}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.pageTitle}>Escolha seu produto</Text>
 
-        <View style={styles.container}>
-            <View>
-                <Text>HOME PAGE</Text>
-                <TouchableOpacity>
-
-                    <AntDesign
-                        name="shoppingcart"
-                        size={25}
-                        color="black"
-                        onPress={() => navigator.navigate('cart', { item: cartItems })}
-                    />
-
-                </TouchableOpacity>
-                <AntDesign />
+                    <TouchableOpacity>
+                        <AntDesign
+                            name="shoppingcart"
+                            size={30}
+                            color="black"
+                            style={styles.cartIcon}
+                            onPress={() => navigator.navigate('cart', { item: cartItems })}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
 
-            {products.map(product => {
+            <View style={styles.container}>
+                {products.map(product => {
 
-                return (
-                    <View key={product.id} style={styles.productView}>
-                        <Image style={{ width: 100, height: 100 }} source={{ uri: product.image }} />
-                        <Text>{product.title}</Text>
-                        <Text>{product.price}</Text>
+                    return (
 
-                        <Button
-                            title={"Add to Cart"}
-                            color={"green"}
-                            onPress={() => {
-                                buttonOnPress(product, product.id)
-                            }}
-                        />
+                        <View key={product.id} style={styles.productView}>
+                            <View style={styles.productImage}>
+                                <Image style={{ width: 100, height: 100 }} source={{ uri: product.image }} />
+                            </View>
+                            <View style={styles.productInfo}>
+                                <Text numberOfLines={1} style={styles.productTitle}>{product.title.length < 30 ? product.title : `${product.title.substring(0, 25)}...`}</Text>
+                                <Text style={styles.productPrice}>{`$${product.price}`}</Text>
+                                <View style={styles.buttonsView}>
+                                    <Ionicons
+                                        name="remove-circle-outline"
+                                        size={30}
+                                        color="red"
+                                        onPress={() => {
+                                            removeFromCart(product)
+                                        }}
+                                    />
+                                    <Ionicons
+                                        name="add-circle-outline"
+                                        size={30}
+                                        color="green"
+                                        onPress={() => {
+                                            addToCart(cartItems, product)
+                                        }}
+                                    />
+                                </View>
+                            </View>
+                        </View>
 
-                    </View>
-                );
-            })}
+                    );
+                })}
+            </View>
         </View>
 
     )
@@ -105,25 +138,70 @@ export default Products
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
+        alignItems: "center"
 
     },
-
+    firstContainer: {
+        alignItems: "center"
+    },
+    titleContainer: {
+        margin: "0 auto",
+        width: "95%",
+        display: 'flex',
+        position: "relative",
+        flexDirection: "row",
+        padding: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        borderBottomWidth: 1,
+        borderColor: "#d9d9d9"
+    },
+    pageTitle: {
+        fontFamily: "Roboto_400Regular",
+        fontSize: 20,
+        fontWeight: "bold",
+        marginRight: "35%",
+        color: "#B84A62"
+    },
+    cartIcon: {
+        color: "#B84A62"
+    },
     productView: {
-        width: "40%",
+        width: '90%',
+        height: 150,
         alignItems: 'center',
-        flexDirection: 'column',
+        flexDirection: 'row',
         backgroundColor: "white",
         padding: 20,
-        marginVertical: 20,
+        marginTop: 20,
         marginHorizontal: 10,
+        borderRadius: 15
     },
-    viewButtonTrue: {
-        display: 'flex'
-    },
-    viewButtonFalse: {
-        display: 'none'
-    }
+    productImage: {
+        width: '40%',
+        borderRightWidth: 1,
+        borderColor: "#d9d9d9"
 
+    },
+    productInfo: {
+        width: '60%',
+        alignItems: 'center',
+    },
+    productTitle: {
+        fontFamily: "Roboto_400Regular",
+        color: "#54457F",
+        fontSize: 15
+    },
+    productPrice: {
+        fontFamily: "Roboto_400Regular",
+        fontWeight: "bold",
+        color: "#54457F",
+        fontSize: 25,
+        paddingVertical: 10
+    },
+    buttonsView: {
+        flexDirection: "row",
+    }
 })
 
 
